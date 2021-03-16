759c447967bd0a42dccd682faefae505f3815951
uyousafzai54 <umaryousafzai54@gmail.com>
2021-03-16 17:07:20 -0400

    more styling

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   const url = process.env.REACT_APP_AUTH0_CLIENT_URL;
   return (
     <div className="nav-container mb-3">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href={url}>Hack the North</a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
