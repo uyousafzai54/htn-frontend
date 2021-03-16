714d5bcb067a01934cc29914950df84354a61e12
uyousafzai54 <umaryousafzai54@gmail.com>
2021-03-16 17:15:30 -0400

    even more styling fixes

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 //import MainNav from "./main-nav";
 import AuthNav from "./auth-nav";
+import logo from '../assets/htn.ico';
 
 const NavBar = () => {
   const url = process.env.REACT_APP_AUTH0_CLIENT_URL;
   return (
     <div className="nav-container mb-3">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href={url}>Hack the North</a>
+        <a class="navbar-brand" href={url}>
+          <img src = {logo} width = "30" height = "30" alt = ""></img>
+        </a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
       <a class="nav-item nav-link" href={`${url}events`}>Events</a>
